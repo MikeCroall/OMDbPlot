1744b7d6672cf1bf0459d400dcdfa6697d4f74a5
mikecroall <mikebcroall@gmail.com>
2016-07-11 23:59:24 +0100

    Added progress indicator to data collection

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from time import sleep
+
+
 def collect_data():
     import random  # todo remove
     d = {}
     start_year, end_year = 1950, 2017  # ensures all relevant titles, basically
+    total_years = end_year-start_year
     print("\tquerying from {} to {}".format(start_year, end_year))
     for year in range(start_year, end_year + 1):
         # todo api call to get list of ratings, average them
         d[year] = random.randint(0, 100)
+        print("\r\t{:.2f}% complete...".format(100 * (year-start_year) / total_years), end='')
+    print("\r\t100% complete")
 
     # data automatically displays in order because of the nature of the dictionary
     # do not worry about sorting at any point
@@ -20,11 +26,10 @@ def create_graph(plt, data):
     years = [i for i in data.keys()]
     ratings = [i for i in data.values()]
     plt.bar(years, ratings, width, color="green", align="center", alpha=opacity)
-    plt.xlabel('Year')
-    plt.ylabel('Average rating over all films from given year')
-    plt.title(
-        'Average film rating (out of {}) by year of release'.format('{{placeholder}}'))  # todo replace placeholder
     plt.xticks(years, data.keys(), rotation='vertical')
+    plt.xlabel('Year of release')
+    plt.ylabel('Average rating over all films released during given year')
+    plt.title('Average film rating (out of {}) by year of release'.format('{{placeholder}}'))  # todo replace placeholder
 
 
 def main():
