6bb10e615e5b99fb5ccf1d86d0220c5019df6033
mikecroall <mikebcroall@gmail.com>
2016-07-14 17:51:48 +0100

    Ignore created

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ def collect_data():
     import random  # todo remove
     d = {}
     start_year, end_year = 1950, 2017  # ensures all relevant titles, basically
-    total_years = end_year-start_year
+    total_years = end_year - start_year
     print("\tquerying from {} to {}".format(start_year, end_year))
     for year in range(start_year, end_year + 1):
-        # todo api call to get list of ratings, average them
-        d[year] = random.randint(0, 100)
-        print("\r\t{:.2f}% complete...".format(100 * (year-start_year) / total_years), end='')
+        d[year] = random.randint(0, 100)  # todo api call to get list of ratings, average them
+        print("\r\t{:.2f}% complete...".format(100 * (year - start_year) / total_years), end='')
     print("\r\t100% complete")
 
     # data automatically displays in order because of the nature of the dictionary
@@ -29,7 +28,8 @@ def create_graph(plt, data):
     plt.xticks(years, data.keys(), rotation='vertical')
     plt.xlabel('Year of release')
     plt.ylabel('Average rating over all films released during given year')
-    plt.title('Average film rating (out of {}) by year of release'.format('{{placeholder}}'))  # todo replace placeholder
+    plt.title('Average film rating (out of {}) by year of release'.format('{{placeholder}}'))
+    # todo replace placeholder above
 
 
 def main():
